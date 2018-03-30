f4bb090e8a1184adfe44f4a89b6a2f35e0b25f14
WeR58 <wer58@ya.ru>
2018-03-30 15:49:51 +0400

    Added noty in findFriend.js. Fix cascade delete, but don't solve transactional problem. Added test for root controller

---
+++ src/test/java/com/pet_space/controllers/RootControllerIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.pet_space.controllers;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static com.pet_space.repositories.UserEssenceRepositoryTestData.USER_ESSENCE_FRED;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+public class RootControllerIntegrationTest extends ControllerInit {
+    @Test
+    public void getRootView() {
+        when(this.authentication.getName()).thenReturn(USER_ESSENCE_FRED.getNickname());
+        new RootController(this.userEssenceRepository, this.customUserEssenceRepository).getRootView(this.authentication, this.mockHttpSession);
+        assertNotNull(this.mockHttpSession.getValue("users"));
+        assertThat(this.mockHttpSession.getValue("users"), is(this.userEssenceRepository.findAll()));
+    }
+}
